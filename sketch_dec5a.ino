c3d8720822dc90caaaf152f19aa8dd00e6b6adeb
achmichael <155420181+achmichael@users.noreply.github.com>
2024-12-09 01:24:55 +0700

    add sensor ds18b20 sketch_dec5a.ino

--- sketch_dec5a.ino
+++ sketch_dec5a.ino
@@ -6,6 +6,7 @@
 #include <Servo.h>
 
 
+#define RELAY_PIN 1
 #define DS18B20_PIN 2
 #define DHT22_PIN 3
 #define BUTTON_START 5 
@@ -62,6 +63,7 @@ void setup() {
   pinMode(BUTTON_START, INPUT_PULLUP);
   pinMode(BUTTON_POWER, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
 
   digitalWrite(BUZZER_PIN, HIGH);
 
@@ -82,23 +84,28 @@ void setup() {
 }
 
 void loop() {
-  // Pembacaan tombol start
   int buttonStart = digitalRead(BUTTON_POWER);
+  sensors.requestTemperatures();
+   // Mengambil nilai suhu dalam Celcius
+  float temperatureC = sensors.getTempCByIndex(0);
 
-  // Toggle system on/off
+  // Mengambil nilai suhu dalam Fahrenheit (opsional)
+  float temperatureF = sensors.getTempFByIndex(0);
+
+   if (temperatureC != DEVICE_DISCONNECTED_C) { // Memeriksa apakah sensor terhubung
+    Serial.print(temperatureC);
+    Serial.print(" °C");
+    Serial.print(" / ");
+    Serial.print(temperatureF);
+    Serial.println(" °F");
+  } else {
+    Serial.println("Sensor tidak terhubung!");
+  }
+
   if (buttonStart == LOW && oldButtonStart == HIGH) {
     systemOn = !systemOn;
 
     if (systemOn) {
-      teaServo.write(80);
-      delay(2000);
-
-       teaServo.write(120);
-      delay(2000);
-
-       teaServo.write(180);
-      delay(2000);
-
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Sistem Aktif");
@@ -108,7 +115,6 @@ void loop() {
       delay(1000);
       digitalWrite(BUZZER_PIN, HIGH);
     } else {
-      // Reset semua variabel saat sistem dimatikan
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Tekan Start");
@@ -123,21 +129,19 @@ void loop() {
   }
 
   oldButtonStart = buttonStart;
-  // Proses hanya jika sistem aktif
   if (systemOn) {
-    // Pembacaan nilai potentiometer untuk menentukan waktu perebusan
     potValue = analogRead(POT_PIN);
     
     // Mengecek jika ada perubahan signifikan pada nilai potentiometer
-    // if (abs(potValue - previousPotValue) > 5 && !brewing) {  // Jika perubahan lebih besar dari 5 (threshold)
+    if (abs(potValue - previousPotValue) > 5 && !brewing) {  // Jika perubahan lebih besar dari 5 (threshold)
       previousPotValue = potValue;  // Menyimpan nilai potentiometer terbaru
     
       // Mengonversi nilai dari potentiometer menjadi waktu (misal 0-1023 -> 0-300 detik)
       boilingTime = map(potValue, 0, 1023, 0, 300);  // Mengubah range potentiometer menjadi 0 - 300 detik (1 - 5 menit)
     
       // Debugging nilai waktu perebusan dalam menit dan detik
-      // int minutes = boilingTime / 60;        // Menghitung menit
-      // int seconds = boilingTime % 60;        // Menghitung sisa detik
+      int minutes = boilingTime / 60;        // Menghitung menit
+      int seconds = boilingTime % 60;        // Menghitung sisa detik
       // Serial.print("Minutes: ");
       // Serial.print(minutes);
       // Serial.print(", Seconds: ");
@@ -148,11 +152,11 @@ void loop() {
       // lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Waktu: ");
-      // lcd.print(minutes);
+      lcd.print(minutes);
       lcd.print("m ");
-      // lcd.print(seconds);
+      lcd.print(seconds);
       lcd.print("s");
-    // }
+    }
 
     float temperature = dht.readTemperature();
     float humidity = dht.readHumidity();
@@ -170,28 +174,45 @@ void loop() {
 
     // Pembacaan tombol start untuk memulai proses perebusan
     int startButton = digitalRead(BUTTON_START);
+    Serial.println("Button start " + String(startButton));
+    delay(2000);
     if (startButton == LOW && !brewing) {
-      brewing = true;  
-      Serial.println("Proses rebus dimulai");
-      for (int i = 0; i <= 180; i ++){
-        teaServo.write(i);
-        delay(15);
-      }
+      brewing = true; 
 
-      for (int i = 180; i >= 0; i --){
-        teaServo.write(i);
-        delay(15);
-      }
+      lcd.setCursor(0, 0); 
+      lcd.println("Proses rebus dimulai");
 
       Serial.print("Durasi: ");
       Serial.println(boilingTime);
+      digitalWrite(RELAY_PIN, LOW);
       accurateDelay(boilingTime / 60.0);  // Memulai proses perebusan berdasarkan waktu yang ditentukan
+      digitalWrite(RELAY_PIN, HIGH);
       digitalWrite(BUZZER_PIN, LOW);
+       for (int i = 0; i <= 180; i ++){
+        teaServo.write(i);
+        delay(15);
+      }
       tone(BUZZER_PIN, 5, 100);
       delay(1000);
       digitalWrite(BUZZER_PIN, HIGH);
+
       lcd.setCursor(0, 0);
       lcd.print("Brewing selesai");
+      delay(200);
+      lcd.setCursor(0, 0);
+      lcd.println("Suhu air " + String(temperatureC) + " C");
+      delay(200);
+      if (temperatureC > 57.8) {
+        digitalWrite(BUZZER_PIN, LOW);
+        tone(BUZZER_PIN, 5, 100);
+        delay(1000);
+        digitalWrite(BUZZER_PIN, HIGH);
+        lcd.setCursor(0, 0);
+        lcd.println("Air berbahaya untuk diminum");
+        delay(200);
+        lcd.setCursor(0, 0);
+        lcd.println("Karena panas!");
+      }
       brewing = false;
       boilingTime = 0;
     }
